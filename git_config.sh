4c5f186fb9a080f02b5a44ee59c7b6592749b80e
Kapralc <zarskypatrik@seznam.cz>
2024-09-24 07:47:42 +0200

    feat: :fire: login

--- git_config.sh
+++ git_config.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+git config --list
 git config --global user.name "Kapralc"
 git config --global user.email "zarskypatrik@seznam.cz"
 echo "Přihlášení uživatele proběhlo"
+ls
+
+
 ##cd eshop_expSkero
+## fontshare.com
