5791fb933038aa98f329bf850b0f2b42477fc414
Kapralc <zarskypatrik@seznam.cz>
2024-10-01 08:05:35 +0200

    feat: :fire: opa

--- git_config.sh
+++ git_config.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+git config --list
+git config --global --unset-all user.name
+git config --global --unset-all user.email
 git config --global user.name "Kapralc"
 git config --global user.email "zarskypatrik@seznam.cz"
 echo "Přihlášení uživatele proběhlo"
+
+ls
+
+
 ##cd eshop_expSkero
+#php artisan serve
